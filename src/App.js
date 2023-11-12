89039c1e0f06102f05e7aa2880166a260ae73acd
vladimir817vk@gmail.com <vladimir817vk@gmail.com>
2023-11-13 00:14:44 +0300

    add fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from "react";
-import {useQuery} from "@apollo/client";
-import {GET_ALL_USERS} from "./query/user";
+import {useMutation, useQuery} from "@apollo/client";
+import {GET_ALL_USERS, GET_USER} from "./query/user";
+import {CREATE_USER} from "./mutations/user";
 
 function App() {
-    const {data, loading, error} = useQuery(GET_ALL_USERS)
+    const {data, loading, error, refetch} = useQuery(GET_ALL_USERS, {pollInterval: 500})
+    const {data: oneUser, loading: loadingOneUser} = useQuery(GET_USER, {
+        variables: {
+            id: 1
+        }
+    })
+    const [newUser] = useMutation(CREATE_USER)
     const [users, setUsers] = useState([])
+    const [username, setUsername] = useState('')
+    const [age, setAge] = useState(0)
+
     console.log(data)
+    console.log(oneUser)
     useEffect(() => {
         if(!loading) {
             setUsers(data.getAllUsers)
         }
     }, [data])
 
+    const addUser = (e) => {
+        e.preventDefault()
+        newUser({
+            variables: {
+                input: {
+                    username, age
+                }
+            }
+        }).then(({data}) => {
+            console.log(data)
+            setUsername('')
+            setAge(0)
+        })
+    }
+
+    const getAll = (e) => {
+        e.preventDefault()
+        refetch()
+    }
+
   return (
     <div>
       <form action="">
-        <input type="text"/>
-        <input type="number"/>
-          <button>Create</button>
-          <button>Get</button>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
+        <input type="number" value={age} onChange={(e) => setAge(e.target.value)}/>
+          <button onClick={(e) => addUser(e)}>Create</button>
+          <button onClick={(e) => getAll(e)}>Get</button>
       </form>
         <div>
             Users:
--- src/query/user.js
+++ src/query/user.js
@@ -7,3 +7,11 @@ export const GET_ALL_USERS = gql`
         }
     }
 `
+
+export const GET_USER = gql`
+    query getUser($id: ID){
+        getUser(id: $id) {
+            id, username, age
+        }
+    }
+`
