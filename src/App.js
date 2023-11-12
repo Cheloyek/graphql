acfc71c28297214d17dce41546e1b4a791230fd5
vladimir817vk@gmail.com <vladimir817vk@gmail.com>
2023-11-12 23:47:42 +0300

    add client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {useQuery} from "@apollo/client";
+import {GET_ALL_USERS} from "./query/user";
 
 function App() {
+    const {data, loading, error} = useQuery(GET_ALL_USERS)
+    const [users, setUsers] = useState([])
+    console.log(data)
+    useEffect(() => {
+        if(!loading) {
+            setUsers(data.getAllUsers)
+        }
+    }, [data])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form action="">
+        <input type="text"/>
+        <input type="number"/>
+          <button>Create</button>
+          <button>Get</button>
+      </form>
+        <div>
+            Users:
+            {users.map(user => <div>{user.id}) {user.username} {user.age}</div>)}
+        </div>
     </div>
   );
 }
